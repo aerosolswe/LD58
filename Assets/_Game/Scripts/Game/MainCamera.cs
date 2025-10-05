357ecabce950d2c443825ae1702372e32baa450c
Theodore Andersson <aerosolswe@gmail.com>
2025-10-05 02:45:10 +0200

    Fixes

--- Assets/_Game/Scripts/Game/MainCamera.cs
+++ Assets/_Game/Scripts/Game/MainCamera.cs
@@ -5,6 +5,7 @@ public class MainCamera : MonoBehaviour
     public Camera Cam;
     public Truck Truck;
 
+    public float XOffset = -1.0f;
     public float YOffset = 2.0f;
     public float LerpSpeed = 25.0f;
 
@@ -14,8 +15,9 @@ public class MainCamera : MonoBehaviour
         var truckPosition = Truck.transform.position;
 
         var newPosition = Vector3.Lerp(currentPosition, truckPosition, LerpSpeed * Time.fixedDeltaTime);
-        newPosition.z = -10;
+        newPosition.x += XOffset;
         newPosition.y += YOffset;
+        newPosition.z = -10;
         transform.position = newPosition;
     }
 }
