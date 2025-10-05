108713a4c2f8aa80225ff7575fca59e746b740e9
Theodore Andersson <aerosolswe@gmail.com>
2025-10-05 17:08:29 +0200

    Update TruckSideLoader.cs

--- Assets/_Game/Scripts/Game/Truck/TruckSideLoader.cs
+++ Assets/_Game/Scripts/Game/Truck/TruckSideLoader.cs
@@ -22,6 +22,7 @@ public class TruckSideLoader : MonoBehaviour
 
         Busy = true;
         TriggerCollider.enabled = false;
+        Animation.Stop();
         Animation.Play();
         trash.Renderer.enabled = false;
         trash.Body.bodyType = RigidbodyType2D.Kinematic;
